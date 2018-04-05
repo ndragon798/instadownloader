4dddd392098986752c4627ae0c2c4052966e743f
Nathan <ndragon798@yahoo.com>
2018-04-04 21:40:23 -0400

    Clean up output a bit and add some comments so you can follow the code better

--- getfollowing.py
+++ getfollowing.py
@@ -12,34 +12,37 @@ def scrolldown():
 	time.sleep(1)
 	webdriver.ActionChains(driver).send_keys(Keys.PAGE_DOWN).perform()
 	time.sleep(1)
-	print(len(driver.find_elements_by_tag_name("li")))
+	# print(len(driver.find_elements_by_tag_name("li")))
 	return len(driver.find_elements_by_tag_name("li"))
-
+#Read in username and password for instagram
 username_ = input("Please Input Username: ").strip()
 password_ = getpass.getpass("Please Input Password: ").strip()
 
 
 # Optional argument, if not specified will search path.
-
+#Setup headless chrome for selenium
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('headless')
 
 driver = webdriver.Chrome('./chromedriver',chrome_options=chrome_options)
-# driver = webdriver.Chrome('./chromedriver')
+#Load login page
 driver.get('https://www.instagram.com/accounts/login/')
-# time.sleep(5) # Let the user actually see something!
+# Type in login 
 username_box = driver.find_element_by_name('username')
 username_box.send_keys(username_)
 password_box = driver.find_element_by_name('password')
 password_box.send_keys(password_)
-
-
 password_box.submit()
+
+#Wait for login
 time.sleep(5)
+#Close popup
 webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+#Go to profile
 link = driver.find_element_by_link_text('Profile')
 link.click()
 time.sleep(3)
+# Open following tab
 links = driver.find_elements_by_tag_name('a')
 for i in links:
 	if 'following' in i.get_attribute('href'):
@@ -47,21 +50,25 @@ for i in links:
 		i.click()
 webdriver.ActionChains(driver).send_keys(Keys.TAB).perform()
 webdriver.ActionChains(driver).send_keys(Keys.TAB).perform()
-
+#Find inital amount of loaded following
 asdf = len(driver.find_elements_by_tag_name("li"))
 asdfprevious = -1
 print(asdf, asdfprevious)
+#Scroll all the way down the following list
 while (asdf != asdfprevious):
 	asdfprevious = asdf
 	asdf = scrolldown()
 	print(asdf, asdfprevious)
 followinglist = []
+#Grab all user names and images
 atext = driver.find_elements_by_xpath("//li/div/div/div/div/a")
 img=driver.find_elements_by_xpath("//a/img")
 
 for i in atext:
 	print(i.text)
 	followinglist.append(i.text)
+
+#Create thumbs and users directories
 ThumbsFilePath='./thumbs/'
 if not os.path.exists(ThumbsFilePath):
     os.makedirs(ThumbsFilePath)
@@ -69,13 +76,19 @@ UserFilePath='./users/'
 if not os.path.exists(UserFilePath):
 	os.makedirs(UserFilePath)
 img_src=[]
+#Grab all the thumbnails
 for i in range(0,len(img)):
 	img_src.append(img[i].get_attribute('src'))
-	print(img_src)
+	# print(img_src)
 	os.system('wget -q -O '+ThumbsFilePath+followinglist[i]+'.jpg '+img_src[i]+' &')
+
+#Close selenium
 driver.quit()
+#Login into instagram
 looter=ProfileLooter("instagram")
 looter.login(username_,password_)
+
+#Loop through all the people who are being followed and grab their photo urls
 for i in followinglist:
 
 	print(i)
@@ -90,9 +103,11 @@ for i in followinglist:
 		else:
 			# print(media)
 			url = media['display_url']
-		# print(url)
+			# print(url)
 		if url.strip()[-2:] != '.1':
 			with open(UserFilePath+i, "a") as output:
+				#Output the file urls to wget-able format
 				output.write("{}\n".format(url))
-	time.sleep(2.5)
+	time.sleep(4)
+	#Wget from the file
 	os.system('wget -q -i '+UserFilePath+i+' -P '+UserFilePath+i+' &')
